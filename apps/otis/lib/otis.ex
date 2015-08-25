2a9e5d7d37a3236de7698a15a681f7261c002f66
Garry Hill <garry@magnetised.net>
2015-08-25 22:04:53 +0100

    I can use multiples of 147 frames...
    Makes everything more reliable

--- apps/otis/lib/otis.ex
+++ apps/otis/lib/otis.ex
@@ -10,9 +10,17 @@ defmodule Otis do
 
   # Final bitrate  is 176,400 Bytes/s
   # So 176400 = @stream_bytes_per_step * ( 1000 / @stream_interval_ms)
-  @stream_frames_per_step 147
-  @stream_bytes_per_step  3528
-  @stream_interval_ms     20
+  # These numbers are chosen so that the gap between frames is some integer
+  # number of milliseconds.
+
+  # The higher the @multiplier the bigger each individual frame is, the longer
+  # the gap between frames, the less work the server has to do and (probably)
+  # the more reliable and the stream is *but* the longer the gap between
+  # pressing stop and hearing the music stop
+  @multiplier             16
+  @stream_frames_per_step 147  * @multiplier
+  @stream_bytes_per_step  3528 * @multiplier
+  @stream_interval_ms     20   * @multiplier
 
   def sample_freq, do: @sample_freq
   def sample_bits, do: @sample_bits
