570835e178cc0eec86231a4ba5261ab209973a71
Garry Hill <garry@magnetised.net>
2015-10-21 21:20:20 +0100

    Enable setting a receiver's volume

--- apps/otis/lib/otis/receiver.ex
+++ apps/otis/lib/otis/receiver.ex
@@ -7,7 +7,8 @@ defmodule Otis.Receiver do
               name: "Receiver",
               latency: nil,
               channel_monitor: nil,
-              zone: nil
+              zone: nil,
+              volume: 1.0
   end
 
   defp receiver_register_name(id) do
@@ -51,12 +52,24 @@ defmodule Otis.Receiver do
     GenServer.cast(pid, :shutdown)
   end
 
+  def volume(pid) do
+    GenServer.call(pid, :get_volume)
+  end
+
+  def volume(pid, volume) do
+    GenServer.cast(pid, {:set_volume, volume})
+  end
+
   def handle_call(:id, _from, %S{id: id} = receiver) do
     {:reply, {:ok, id}, receiver}
   end
 
   def handle_call(:get_latency, _from, %S{latency: latency} = receiver) do
     {:reply, {:ok, latency}, receiver}
+  end
+
+  def handle_call(:get_volume, _from, %S{volume: volume} = receiver) do
+    {:reply, {:ok, volume}, receiver}
   end
 
   def handle_cast({:update_latency, latency}, %S{id: id, latency: nil} = state) do
@@ -70,16 +83,19 @@ defmodule Otis.Receiver do
     {:noreply, %S{state | latency: l}}
   end
 
-  def handle_cast({:join_zone, zone}, %S{id: id} = state) do
+  def handle_cast({:join_zone, zone}, state) do
     {:ok, {ip, port}} = Otis.Zone.broadcast_address(zone)
     # Now I want to send the ip:port info to the receiver which should cause it
     # to launch a player instance attached to that udp address (along with the
     # necessary linked processes)
-    # GenServer.cast({Janis.Monitor, node}, {:join_zone, {ip, port}, Otis.stream_interval_ms, Otis.stream_bytes_per_step})
-    channel = "receiver:" <> Atom.to_string(id)
-    Logger.debug "Receiver joining zone #{inspect {ip, port}} #{channel}"
-    Elvis.Endpoint.broadcast!(channel, "join_zone", %{address: Tuple.to_list(ip), port: port, interval: Otis.stream_interval_ms, size: Otis.stream_bytes_per_step})
+    broadcast!(state, "join_zone", %{address: Tuple.to_list(ip), port: port, interval: Otis.stream_interval_ms, size: Otis.stream_bytes_per_step, volume: 1.0})
     {:noreply, %S{ state | zone: zone }}
+  end
+
+  def handle_cast({:set_volume, volume}, state) do
+    volume = sanitize_volume(volume)
+    broadcast!(state, "set_volume", %{volume: volume})
+    {:noreply, %S{ state | volume: volume }}
   end
 
   def handle_cast(:shutdown, %S{id: id, zone: zone} = state) do
@@ -101,5 +117,24 @@ defmodule Otis.Receiver do
   def terminate(reason, _state) do
     IO.inspect [:receiver_terminate, reason]
     :ok
+  end
+
+  defp broadcast!(state, event, msg) do
+    Logger.debug "Sending event #{channel_name(state)} -> #{inspect event} :: #{ inspect msg }"
+    Elvis.Endpoint.broadcast!(channel_name(state), event, msg)
+  end
+
+  defp channel_name(%S{id: id}) do
+    "receiver:" <> Atom.to_string(id)
+  end
+
+  defp sanitize_volume(volume) when volume > 1.0 do
+    1.0
+  end
+  defp sanitize_volume(volume) when volume < 0.0 do
+    0.0
+  end
+  defp sanitize_volume(volume) do
+    volume
   end
 end
