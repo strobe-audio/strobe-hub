d5ccd0da65ad1da0b89640eac9c57b7da1f78b27
Garry Hill <garry@magnetised.net>
2015-10-27 17:20:40 +0000

    Enable pre-buffering of current source

--- apps/otis/lib/otis/source/file.ex
+++ apps/otis/lib/otis/source/file.ex
@@ -7,7 +7,14 @@ defmodule Otis.Source.File do
   end
 
   def start_link(path) do
-    GenServer.start_link(__MODULE__, %{path: path, outputstream: nil, inputstream: nil, transcode_pid: nil})
+    state = %{
+      path: path,
+      outputstream: nil,
+      inputstream: nil,
+      transcode_pid: nil,
+      pending_streams: nil
+    }
+    GenServer.start_link(__MODULE__, state)
   end
 
   def handle_call(:chunk, _from, source) do
@@ -23,8 +30,12 @@ defmodule Otis.Source.File do
     {:noreply, open(source)}
   end
 
-  defp chunk(%{outputstream: nil} = source) do
+  defp chunk(%{outputstream: nil, pending_streams: nil} = source) do
     chunk(open(source))
+  end
+
+  defp chunk(%{outputstream: nil, pending_streams: {inputstream, outputstream, pid}} = source) do
+    chunk(%{source | inputstream: inputstream, outputstream: outputstream, transcode_pid: pid, pending_streams: nil })
   end
 
   defp chunk(%{outputstream: outputstream} = source) do
@@ -36,14 +47,19 @@ defmodule Otis.Source.File do
   end
 
   defp next_chunk(:error, %{inputstream: inputstream} = source) do
+    # TODO: can I tell the transcode process to exit or will it just get GC'd
     Elixir.File.close(inputstream)
-    {:reply, :done, %{source | inputstream: nil, outputstream: nil}}
+    {:reply, :done, %{source | inputstream: nil, outputstream: nil, transcode_pid: nil}}
+  end
+
+  defp open(%{pending_streams: nil} = source) do
+    inputstream = input_stream(source)
+    { pid, outputstream } = Otis.Transcoders.Avconv.transcode(inputstream, file_type(source))
+    %{source | pending_streams: {inputstream, outputstream, pid} }
   end
 
   defp open(source) do
-    inputstream = input_stream(source)
-    { pid, outputstream } = Otis.Transcoders.Avconv.transcode(inputstream, file_type(source))
-    %{source | inputstream: inputstream, outputstream: outputstream, transcode_pid: pid }
+    source
   end
 
   defp input_stream(%{ path: path }) do
