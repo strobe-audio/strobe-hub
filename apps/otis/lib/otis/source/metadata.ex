e02d8a6c5d268ae153d2877a5a5fd53df1b7f457
Garry Hill <garry@magnetised.net>
2015-11-12 21:06:47 +0000

    Reorganize

--- apps/otis/lib/otis/source/metadata.ex
+++ apps/otis/lib/otis/source/metadata.ex
@@ -1,22 +1,25 @@
 defmodule Otis.Source.Metadata do
   defstruct [
-    :album,
+    # Technical
     :bit_rate,
     :channels,
+    :duration_ms,
+    :extension,
+    :filename,
+    :mime_type,
+    :sample_rate,
+    :stream_size,
+    # Cultural
+    :album,
     :composer,
     :date,
     :disk_number,
     :disk_total,
-    :duration_ms,
-    :extension,
-    :filename,
     :genre,
-    :mime_type,
     :performer,
-    :sample_rate,
-    :stream_size,
     :title,
     :track_number,
     :track_total
   ]
 end
+
