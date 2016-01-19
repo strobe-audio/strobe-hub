0f4ca7ff830aa30d5a359805b2edce7fd942230c
Garry Hill <garry@magnetised.net>
2016-01-19 09:31:14 +0000

    Changes for elixir 1.2

--- apps/elvis/mix.exs
+++ apps/elvis/mix.exs
@@ -4,6 +4,8 @@ defmodule Elvis.Mixfile do
   def project do
     [app: :elvis,
      version: "0.0.1",
+     build_path: "../../_build",
+     # config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
      elixir: "~> 1.0",
