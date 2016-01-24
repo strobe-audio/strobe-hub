4268f56f83e4b47b87a6d7ffb6c30e54a32c1d9a
Garry Hill <garry@magnetised.net>
2016-01-24 20:24:20 +0000

    Improve source list test

--- apps/otis/test/otis/source_list_test.exs
+++ apps/otis/test/otis/source_list_test.exs
@@ -24,6 +24,34 @@ defmodule Otis.SourceListTest do
     {:ok, sources: sources, source_list: list}
   end
 
+  test "it gives each source a unique id", %{source_list: list} do
+    {:ok, sources} = Otis.SourceList.list(list)
+    ids = Enum.map sources, fn({id, _source}) -> id end
+    assert length(Enum.uniq(ids)) == length(ids)
+  end
+
+  test "it gives new sources a unique id", %{source_list: list} do
+    source = TS.new("e")
+    {:ok, sources} = Otis.SourceList.list(list)
+    l = length(sources)
+    Otis.SourceList.append_source(list, source)
+    {:ok, sources} = Otis.SourceList.list(list)
+    ids = Enum.map sources, fn({id, _source}) -> id end
+    assert length(Enum.uniq(ids)) == length(ids)
+    assert length(ids) == l + 1
+  end
+
+  test "it gives new multiple sources unique ids", %{source_list: list} do
+    new_sources = [TS.new("e"), TS.new("f")]
+    {:ok, sources} = Otis.SourceList.list(list)
+    l = length(sources)
+    Otis.SourceList.append_sources(list, new_sources)
+    {:ok, sources} = Otis.SourceList.list(list)
+    ids = Enum.map sources, fn({id, _source}) -> id end
+    assert length(Enum.uniq(ids)) == length(ids)
+    assert length(ids) == l + 2
+  end
+
   test "skips a single track", state do
     {:ok, 3} = Otis.SourceList.skip(state.source_list, 1)
     {:ok, _id, source} = Otis.SourceList.next(state.source_list)
