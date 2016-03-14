8626593e83d1754c8bb191cf145eea422f1c2b89
Garry Hill <garry@magnetised.net>
2016-03-14 09:53:42 +0000

    Disable debug logging for dev
    Easier than slowing down the rate of source position updates

--- apps/otis/config/dev.exs
+++ apps/otis/config/dev.exs
@@ -2,6 +2,13 @@ use Mix.Config
 
 project_root_path = Path.expand(Path.join(__DIR__, "../../.."))
 
+config :logger, :console,
+  level: :info,
+  format: "$date $time $metadata [$level]$levelpad $message\n",
+  sync_threshold: 1_000_000,
+  metadata: [:module, :line],
+  colors: [info: :green]
+
 config :otis, Otis.State.Repo,
   adapter: Sqlite.Ecto,
   database: Path.join(project_root_path, "_state/otis.dev.sqlite3")
