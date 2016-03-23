9695462c5b6bfbb95d8bdca2333e1bef45de4b09
Garry Hill <garry@magnetised.net>
2016-03-23 16:25:01 +0000

    Insert myself as a library when a controller joins

--- apps/peel/lib/peel/events/startup.ex
+++ apps/peel/lib/peel/events/startup.ex
@@ -6,8 +6,11 @@ defmodule Peel.Events.Startup do
     Otis.State.Events.add_mon_handler(__MODULE__, [])
   end
 
-  def handle_event(evt, state) do
-    IO.inspect [:peel, evt]
+  def handle_event({:controller_join, socket}, state) do
+    Otis.State.Events.notify({:add_library, %{name: "Peel"}, socket})
+    {:ok, state}
+  end
+  def handle_event(_event, state) do
     {:ok, state}
   end
 end
