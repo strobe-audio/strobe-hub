2ac1cdeab9e82cef77e0a2900bc5fcb5482278ae
Garry Hill <garry@magnetised.net>
2016-03-25 08:55:09 +0000

    Arghh! The noise!

--- config/config.exs
+++ config/config.exs
@@ -16,6 +16,7 @@ config :elvis, Elvis.Endpoint,
 
 # Configures Elixir's Logger
 config :logger, :console,
+  level: :info,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
