16b3669419f3703026eba62a7bce8ad37fd0bcfe
Garry Hill <garry@magnetised.net>
2016-07-19 16:51:09 +0100

    Ensure the file module is loaded

--- apps/otis/lib/otis/source.ex
+++ apps/otis/lib/otis/source.ex
@@ -35,35 +35,37 @@ defprotocol Otis.Source do
   def duration(source)
 end
 
-defimpl Otis.Source, for: Otis.Source.File do
-  alias Otis.Source.File
+if Code.ensure_loaded?(Otis.Source.File) do
+  defimpl Otis.Source, for: Otis.Source.File do
+    alias Otis.Source.File
 
-  def id(file) do
-    file.id
-  end
+    def id(file) do
+      file.id
+    end
 
-  def type(_file) do
-    Otis.Source.File
-  end
+    def type(_file) do
+      Otis.Source.File
+    end
 
-  def open!(%File{path: path}, packet_size_bytes) do
-    Elixir.File.stream!(path, [], packet_size_bytes)
-  end
+    def open!(%File{path: path}, packet_size_bytes) do
+      Elixir.File.stream!(path, [], packet_size_bytes)
+    end
 
-  def close(%File{}, stream) do
-    Elixir.File.close(stream)
-  end
+    def close(%File{}, stream) do
+      Elixir.File.close(stream)
+    end
 
-  def audio_type(%File{metadata: metadata}) do
-    {metadata.extension, metadata.mime_type}
-  end
+    def audio_type(%File{metadata: metadata}) do
+      {metadata.extension, metadata.mime_type}
+    end
 
-  def metadata(%File{metadata: metadata}) do
-    metadata
-  end
+    def metadata(%File{metadata: metadata}) do
+      metadata
+    end
 
-  def duration(%File{metadata: metadata}) do
-    {:ok, metadata.duration_ms}
+    def duration(%File{metadata: metadata}) do
+      {:ok, metadata.duration_ms}
+    end
   end
 end
 
