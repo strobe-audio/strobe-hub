e2fad6b6c21565a95352b138741e74a3506367fb
Garry Hill <garry@magnetised.net>
2016-07-19 16:43:33 +0100

    Add in retries to the HTTP reader

--- lib/hls/reader/http.ex
+++ lib/hls/reader/http.ex
@@ -1,11 +1,27 @@
 defmodule HLS.Reader.Http do
   defstruct []
+
+  def read(_url, 0, _delay) do
+    {:error, :http}
+  end
+
+  def read(url, attempts, delay) do
+    try do
+      %HTTPoison.Response{status_code: 200, body: body} = HTTPoison.get!(url, [], params: [{"t", now()}])
+      {:ok, body}
+    catch
+:closed, _ ->
+      Process.sleep(delay)
+      read(url, attempts - 1, delay * 2)
+    end
+  end
+
+  defp now, do: :os.system_time(:seconds)
 end
 
 defimpl HLS.Reader, for: HLS.Reader.Http do
   def read!(_reader, url) do
-    %HTTPoison.Response{status_code: 200, body: body} = HTTPoison.get!(url, [], params: [{"t", now()}])
+    {:ok, body} = HLS.Reader.Http.read(url, 5, 100)
     body
   end
-  defp now, do: :os.system_time(:seconds)
 end
