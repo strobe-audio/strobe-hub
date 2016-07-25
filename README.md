6092c5aa291006dc02cdc6b904185d084b79a8f0
Garry Hill <garry@magnetised.net>
2016-07-25 14:42:57 +0100

    Create library

---
+++ README.md
@@ -0,0 +1,24 @@
+# OtisLibrary
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `otis_library` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:otis_library, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `otis_library` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:otis_library]]
+    end
+    ```
+
---
+++ lib/otis/library.ex
@@ -0,0 +1,69 @@
+defmodule Otis.Library do
+  defmacro __using__([namespace: namespace]) do
+    quote location: :keep do
+      @namespace IO.inspect "#{unquote(namespace)}"
+      @protocol IO.inspect "#{unquote(namespace)}:"
+
+      def handle_event({:controller_join, socket}, state) do
+        Otis.State.Events.notify({:add_library, library(), socket})
+        {:ok, state}
+      end
+
+      def handle_event({:library_request, channel_id, @protocol <> path, socket}, state) do
+        route = String.split(path, "/", trim: true)
+        case route_library_request(channel_id, route, path) do
+          nil ->
+            nil
+          response ->
+            Otis.State.Events.notify({:library_response, @namespace, response, socket})
+        end
+        {:ok, state}
+      end
+
+      def handle_event(_event, state) do
+        {:ok, state}
+      end
+
+      def init do
+      end
+
+      def library do
+        %{ id: "invalid",
+          title: "Override me",
+          icon: "",
+          actions: %{
+            click: url("root"),
+            play: nil
+          },
+          metadata: nil
+        }
+      end
+
+      def route_library_request(_channel_id, _route, _path) do
+        nil
+      end
+
+      def library_link(title, action \\ nil)
+      def library_link(title, action) do
+        %{title: title, action: action}
+      end
+
+      def url(path) when is_list(path) do
+        path |> Path.join |> url()
+      end
+
+      def url(path) do
+        "#{@protocol}#{path}"
+      end
+
+      def namespace, do: @namespace
+
+      defoverridable [
+        init: 0,
+        library: 0,
+        route_library_request: 3,
+      ]
+    end
+  end
+end
+
