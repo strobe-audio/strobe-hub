61179f8d08cd4cc5117ef968c3ac64af77181e68
Garry Hill <garry@magnetised.net>
2016-08-24 17:05:42 +0100

    Use git dependency

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Otis.Mixfile do
       {:ranch, "~> 1.0", [optional: false, hex: :ranch, manager: :rebar]},
       {:faker, "~> 0.5", only: :test},
       {:logger_file_backend, "~> 0.0.7"},
-      {:otis_library, path: "/Users/garry/Seafile/Peep/otis_library"},
+      # {:otis_library, path: "/Users/garry/Seafile/Peep/otis_library"},
+      {:otis_library, git: "git@gitlab.com:magnetised/otis_library.git"},
       {:nerves_ssdp_server, "~> 0.2.1"},
       {:gproc, "~> 0.5.0"},
     ]
