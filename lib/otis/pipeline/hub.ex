e31a2d329c155f388db9956a434e83f3933f0b3e
Garry Hill <garry@magnetised.net>
2016-12-15 11:00:25 +0000

    Clean up hub implementation

--- lib/otis/pipeline/hub.ex
+++ lib/otis/pipeline/hub.ex
@@ -1,30 +1,20 @@
 defmodule Otis.Pipeline.Hub do
   use GenServer
-  @moduledoc """
-  The interface between a playlist and the sources
-  1. Takes a playlist
-  2. Pops a rendition from the playlist
-  3. Converts it to a source
-  4. Opens the source
-  5. Puts a transcoder in front of it
-  6. Puts a buffer in front of the transcoder
-  7. Returns packets from the current buffer to whoever wants them
-  8. When a buffer informs us that it's source is done {:done, <<data>>} we do 2-6 to get the next stream
-  9. When the current buffer returns :done we swap in our pending stream with the current one
 
-  When we get a stop, we forward that onto the current stream
-  When we get a skip we clear out our streams and start from scratch
+  @moduledoc """
+  The interface between a broadcaster and a playlist: converts a playlist into
+  a stream of %Packet{}s by creating streams from the playlist entries.
   """
 
   alias Otis.Pipeline.Playlist
   alias Otis.Pipeline.Producer
+  alias Otis.Pipeline.Streams
 
   defmodule S do
     @moduledoc false
     defstruct [
       :playlist,
       :config,
-      :transcoder_module,
       :rendition,
       :stream,
     ]
@@ -43,11 +33,7 @@ defmodule Otis.Pipeline.Hub do
   end
 
   def init([playlist, config]) do
-    state = %S{
-      playlist: playlist,
-      config: config,
-    } |> initialize()
-    {:ok, state}
+    {:ok, %S{playlist: playlist, config: config}}
   end
 
   def handle_call(:next, _from, state) do
@@ -64,6 +50,56 @@ defmodule Otis.Pipeline.Hub do
     {reply, state} = pause_stream(state)
     {:reply, reply, state}
   end
+
+  ## Next handing
+
+  defp next_packet(%S{stream: nil} = state) do
+    state |> load_pending_stream() |> load_packet()
+  end
+  defp next_packet(state) do
+    load_packet({:ok, state})
+  end
+
+  # So we've come from receiving a :done state from our previous stream and
+  # still haven't managed to load a stream, so tell our broadcaster we're done
+  defp load_packet({:done, state}) do
+    {:done, state}
+  end
+  defp load_packet({:ok, %S{stream: stream} = state}) do
+    stream |> Producer.next() |> handle_data(state)
+  end
+
+  defp handle_data({:ok, data}, state) do
+    {{:ok, data}, state}
+  end
+  defp handle_data({:done, data}, state) do
+    {{:ok, data}, state}
+  end
+  defp handle_data(:done, state) do
+    %S{state | stream: nil, rendition: nil} |> load_pending_stream() |> load_packet()
+  end
+
+  # The case when we've paused a live stream
+  defp load_pending_stream(%S{stream: nil, rendition: rendition} = state)
+  when not is_nil(rendition) do
+    {:ok, stream} = start_stream(rendition, state)
+    {:ok, %S{state| stream: stream}}
+  end
+  defp load_pending_stream(state) do
+    case Playlist.next(state.playlist) do
+      {:ok, rendition} ->
+        {:ok, stream} = start_stream(rendition, state)
+        {:ok, %S{ state | stream: stream, rendition: rendition }}
+      :done ->
+        {:done, state}
+    end
+  end
+
+  defp start_stream(rendition, state) do
+    Streams.start_stream(rendition, state.config)
+  end
+
+  ## Pause handling
 
   defp pause_stream(%S{stream: nil} = state) do
     {:ok, state}
@@ -84,69 +120,9 @@ defmodule Otis.Pipeline.Hub do
     %S{state | stream: nil, rendition: nil}
   end
 
-  defp shutdown_producer({stream, _rendition}) do
-    shutdown_producer(stream)
-  end
   defp shutdown_producer(nil) do
   end
   defp shutdown_producer(stream) do
     Producer.stop(stream)
-  end
-
-  defp initialize(state) do
-    # TODO: replace :next with :current which returns either :active or the first
-    case Playlist.next(state.playlist) do
-      {:ok, rendition} ->
-        {:ok, stream} = start_stream(rendition, state)
-        %S{ state | stream: stream, rendition: rendition }
-      :done ->
-        state
-    end
-  end
-
-  defp load_pending_stream(%S{stream: nil, rendition: rendition} = state) when not is_nil(rendition) do
-    {:ok, stream} = start_stream(rendition, state)
-    %S{state| stream: stream}
-  end
-  defp load_pending_stream(state) do
-    case Playlist.next(state.playlist) do
-      {:ok, rendition} ->
-        {:ok, stream} = start_stream(rendition, state)
-        append_stream(stream, rendition, state)
-      :done ->
-        state
-    end
-  end
-
-  defp append_stream(stream, rendition, state) do
-    %S{ state | stream: stream, rendition: rendition }
-  end
-
-  defp start_stream(rendition, state) do
-    Otis.Pipeline.Streams.start_stream(rendition, state.config)
-  end
-
-  defp next_packet(%S{stream: nil} = state) do
-    state |> load_pending_stream() |> next_packet(:done)
-  end
-  defp next_packet(state) do
-    next_packet(state, :ok)
-  end
-  # Prevent infinite loops when coming from an expired stream + playlist
-  defp next_packet(%S{stream: nil} = state, :done) do
-    {:done, state}
-  end
-  defp next_packet(%S{stream: stream} = state, _) do
-    stream |> Producer.next() |> handle_data(state)
-  end
-
-  defp handle_data({:ok, data}, state) do
-    {{:ok, data}, state}
-  end
-  defp handle_data({:done, data}, state) do
-    {{:ok, data}, state}
-  end
-  defp handle_data(:done, state) do
-    %S{state | stream: nil, rendition: nil} |> load_pending_stream() |> next_packet(:done)
   end
 end
