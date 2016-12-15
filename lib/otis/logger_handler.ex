b16d8a55a8e33680402aff9a3901d7f14a3b24e8
Garry Hill <garry@magnetised.net>
2016-12-15 11:01:59 +0000

    Make config a var

--- lib/otis/logger_handler.ex
+++ lib/otis/logger_handler.ex
@@ -2,17 +2,19 @@ defmodule Otis.LoggerHandler do
   use     GenEvent
   require Logger
 
+  @log_progress_every 100
+
   def init(id) do
     {:ok, %{id: id, progress_count: 0}}
   end
 
-  # Rate limit the source progress events to 1 out of 10 (or roughly every 1s)
   def handle_event({:rendition_progress, [_channel_id, _rendition_id, _position, :infinity]}, state) do
     {:ok, state}
   end
+  # Rate limit the source progress events to 1 out of @log_progress_every (or roughly every 10s)
   def handle_event({:rendition_progress, _args} = event, %{progress_count: 0} = state) do
     log_event(event, state)
-    {:ok, %{state | progress_count: 100}}
+    {:ok, %{state | progress_count: @log_progress_every}}
   end
   def handle_event({:rendition_progress, _args}, state) do
     {:ok, %{state | progress_count: state.progress_count - 1}}
