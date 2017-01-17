c35e6ac0fc055cc0697dbc70eb8dc41d553aaa82
Garry Hill <garry@magnetised.net>
2017-01-17 10:17:24 +0000

    Debounce volume change events to limit event spam

--- web/channels/controller_channel.ex
+++ web/channels/controller_channel.ex
@@ -1,21 +1,24 @@
 defmodule Elvis.ControllerChannel do
   use Phoenix.Channel
 
+  @min_volume_change_interval_ms 50
+
   def join("controllers:" <> controller_type, _params, socket) do
     # Don't send the socket with the event because we can't send anything until
     # the channel has been joined (which happens when this function returns)
     # TODO: can use this event to send wakeup calls to the receivers
     Otis.State.Events.notify({:controller_connect, [controller_type]})
     send self(), :controller_join
+    socket = assign_volume_change(socket, now())
     {:ok, socket}
   end
 
   def handle_in("change_volume", ["receiver", id, volume], socket) do
-    Otis.Receivers.volume id, volume
+    socket = test_volume_change_interval(socket, Otis.Receivers, [id, volume])
     {:noreply, socket}
   end
   def handle_in("change_volume", ["channel", id, volume], socket) do
-    Otis.Channels.volume id, volume
+    socket = test_volume_change_interval(socket, Otis.Channels, [id, volume])
     {:noreply, socket}
   end
 
@@ -67,5 +70,28 @@ defmodule Elvis.ControllerChannel do
   def handle_info(:controller_join, socket) do
     Otis.State.Events.notify({:controller_join, [socket]})
     {:noreply, socket}
+  end
+
+  defp test_volume_change_interval(socket, module, args) do
+    time = now()
+    last_event_time = retrieve_volume_change(socket)
+    case time - last_event_time do
+      d when d >= @min_volume_change_interval_ms ->
+        apply(module, :volume, args)
+        assign_volume_change(socket, time)
+      _ -> socket
+    end
+  end
+
+  defp retrieve_volume_change(socket) do
+    socket.assigns.volume_event_time
+  end
+
+  defp assign_volume_change(socket, time) do
+    assign(socket, :volume_event_time, time)
+  end
+
+  defp now do
+    :erlang.monotonic_time(:milli_seconds)
   end
 end
