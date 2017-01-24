88dc219b25ac541b54928c77887847e30e49eced
Garry Hill <garry@magnetised.net>
2017-01-24 16:41:23 +0000

    Remove failing explicit compile option

--- apps/otis/mix.exs
+++ apps/otis/mix.exs
@@ -55,7 +55,7 @@ defmodule Otis.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [ {:porcelain, github: "alco/porcelain"},
-      {:dnssd, github: "benoitc/dnssd_erlang", compile: "rebar compile"},
+      {:dnssd, github: "benoitc/dnssd_erlang"},
       {:poolboy, "~> 1.4"},
       {:monotonic, github: "magnetised/monotonic.ex"},
       {:erlsom, github: "willemdj/erlsom"},
