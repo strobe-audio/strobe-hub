d0f6012b479ecf9e4e8671f79c21bfc73eb9443d
Garry Hill <garry@magnetised.net>
2017-01-24 16:43:27 +0000

    Move conflicting settings out of global config

--- apps/otis/config/dev.exs
+++ apps/otis/config/dev.exs
@@ -1,5 +1,31 @@
 use Mix.Config
 
+config :logger, :debug_log,
+  path: "log/otis.debug.log",
+  level: :debug
+
+config :logger, :console,
+  level: :info,
+  format: "$date $time $metadata [$level]$levelpad $message\n",
+  sync_threshold: 1_000_000,
+  metadata: [:module, :line],
+  colors: [info: :green]
+
+# It is also possible to import configuration files, relative to this
+# directory. For example, you can emulate configuration per environment
+# by uncommenting the line below and defining dev.exs, test.exs and such.
+# Configuration from the imported file will override the ones defined
+# here (which is why it is important to import them last).
+#
+config :porcelain, :driver, Porcelain.Driver.Goon
+config :porcelain, :goon_driver_path, "#{__DIR__}/../bin/goon_darwin_amd64"
+
+config :otis, Otis.Media,
+  root: "#{__DIR__}/../_state/fs",
+  at: "/fs"
+
+import_config "#{Mix.env}.exs"
+
 project_root_path = Path.expand(Path.join(__DIR__, "../../.."))
 
 config :logger, :console,
