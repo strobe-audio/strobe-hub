6a618131f29b81fabed9d25d3c4904f6f356b611
Garry Hill <garry@magnetised.net>
2017-05-03 09:53:53 +0100

    Require Elixir 1.4 across all apps

--- apps/elvis/mix.exs
+++ apps/elvis/mix.exs
@@ -8,7 +8,7 @@ defmodule Elvis.Mixfile do
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
-     elixir: "~> 1.0",
+     elixir: "~> 1.4.0",
      elixirc_paths: elixirc_paths(Mix.env()),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env() == :prod,
