85b822e92319ba09dc9141c60fa78f90642e9361
Garry Hill <garry@magnetised.net>
2017-05-08 11:22:51 +0100

    Compile assets automatically on release

--- apps/strobe_server/rel/plugins/asset_compilation.exs
+++ apps/strobe_server/rel/plugins/asset_compilation.exs
@@ -1,21 +1,55 @@
-IO.inspect [:HERE]
 defmodule Strobe.Server.Release.AssetCompilation do
   use Mix.Releases.Plugin
 
-  def before_assembly(release, opts) do
-    IO.inspect [:before_assembly, release, opts]
+  def before_assembly(release, _opts) do
+    cwd = System.cwd()
+    [__DIR__, "../../../elvis"] |> Path.join() |> Path.expand() |> File.cd()
+    compile_assets()
+    # Go back to where we were
+    cwd |> File.cd()
     release
   end
-  def after_assembly(release, opts) do
-    IO.inspect [:after_assembly, release, opts]
+
+  def after_assembly(release, _opts) do
     release
   end
-  def before_package(release, opts) do
-    IO.inspect [:before_package, release, opts]
+
+  def before_package(release, _opts) do
     release
   end
-  def after_package(release, opts) do
-    IO.inspect [:after_package, release, opts]
+
+  def after_package(release, _opts) do
     release
+  end
+
+  defp compile_assets do
+    bin = case System.cmd(yarn(), ["bin"]) do
+      {path, 0} ->
+        path
+      {_, _} ->
+        :enoent
+    end |> String.strip()
+
+    opts = [
+      stderr_to_stdout: true,
+      into: IO.stream(:stdio, :line),
+    ]
+
+    IO.puts "Compiling JS & CSS..."
+    System.cmd(Path.join([bin, "webpack"]), [
+      "--config",
+      "config/webpack.config.js",
+      "-p",
+    ], opts)
+
+    IO.puts "Generating asset digest..."
+    System.cmd(mix(), ["phoenix.digest"], opts)
+  end
+
+  defp yarn do
+    System.find_executable("yarn")
+  end
+  defp mix do
+    System.find_executable("mix")
   end
 end
