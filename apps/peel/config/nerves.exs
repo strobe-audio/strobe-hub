b7ff1e735b892beeec5cc321154ef9c39db50fad
Garry Hill <garry@magnetised.net>
2017-05-16 11:59:33 +0100

    Reduce logging in embedded mode

--- apps/peel/config/nerves.exs
+++ apps/peel/config/nerves.exs
@@ -1,5 +1,11 @@
 use Mix.Config
 
+config :logger, :console,
+  level: :info,
+  format: "$date $time $metadata [$level]$levelpad $message\n",
+  metadata: [:module, :line],
+  colors: [enabled: false]
+
 config :peel, Peel.Repo,
   adapter: Sqlite.Ecto,
   database: "/state/db/current/peel.sqlite"
