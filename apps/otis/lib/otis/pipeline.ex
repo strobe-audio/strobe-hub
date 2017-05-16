1449b98e9810aa5dbaf9ac97dc34049c76bb642e
Garry Hill <garry@magnetised.net>
2017-05-16 17:04:37 +0100

    Split pipeline processes into separate supervisor
    the pipeline stuff requires :one_for_all but nothing else does

--- apps/otis/lib/otis/pipeline.ex
+++ apps/otis/lib/otis/pipeline.ex
@@ -1,5 +1,29 @@
 defmodule Otis.Pipeline do
+  use Supervisor
+
   def config do
     Otis.Pipeline.Config.new(100)
+  end
+
+  def start_link(pipeline_config) do
+    Supervisor.start_link(__MODULE__, pipeline_config, [])
+  end
+
+  def init(pipeline_config) do
+    children = [
+      supervisor(Registry, [:unique, Otis.Pipeline.Streams.namespace()], id: Otis.Pipeline.Streams.namespace()),
+      supervisor(Otis.Pipeline.Streams, []),
+
+      supervisor(Registry, [:duplicate, Otis.Receivers.Channels.channel_namespace()], id: Otis.Receivers.Channels.channel_namespace()),
+      supervisor(Registry, [:duplicate, Otis.Receivers.Channels.subscriber_namespace()], id: Otis.Receivers.Channels.subscriber_namespace()),
+      supervisor(Otis.Receivers.Channels, []),
+
+      worker(Otis.Receivers.Database, []),
+      worker(Otis.Receivers, [pipeline_config]),
+      worker(Otis.Receivers.Logger, []),
+
+      supervisor(Otis.Channels, []),
+    ]
+    supervise(children, strategy: :one_for_all)
   end
 end
--- apps/otis/lib/otis/supervisor.ex
+++ apps/otis/lib/otis/supervisor.ex
@@ -22,22 +22,23 @@ defmodule Otis.Supervisor do
       worker(Otis.State.Persistence.Renditions, []),
       worker(Otis.State.Persistence.Configuration, []),
 
-      supervisor(Registry, [:unique, Otis.Pipeline.Streams.namespace()], id: Otis.Pipeline.Streams.namespace()),
-      supervisor(Otis.Pipeline.Streams, []),
-
-      supervisor(Registry, [:duplicate, Otis.Receivers.Channels.channel_namespace()], id: Otis.Receivers.Channels.channel_namespace()),
-      supervisor(Registry, [:duplicate, Otis.Receivers.Channels.subscriber_namespace()], id: Otis.Receivers.Channels.subscriber_namespace()),
-      supervisor(Otis.Receivers.Channels, []),
-
-      worker(Otis.Receivers.Database, []),
-      worker(Otis.Receivers, [pipeline_config]),
-      worker(Otis.Receivers.Logger, []),
-
-      supervisor(Otis.Channels, []),
+      # supervisor(Registry, [:unique, Otis.Pipeline.Streams.namespace()], id: Otis.Pipeline.Streams.namespace()),
+      # supervisor(Otis.Pipeline.Streams, []),
+      #
+      # supervisor(Registry, [:duplicate, Otis.Receivers.Channels.channel_namespace()], id: Otis.Receivers.Channels.channel_namespace()),
+      # supervisor(Registry, [:duplicate, Otis.Receivers.Channels.subscriber_namespace()], id: Otis.Receivers.Channels.subscriber_namespace()),
+      # supervisor(Otis.Receivers.Channels, []),
+      #
+      # worker(Otis.Receivers.Database, []),
+      # worker(Otis.Receivers, [pipeline_config]),
+      # worker(Otis.Receivers.Logger, []),
+      #
+      # supervisor(Otis.Channels, []),
+      supervisor(Otis.Pipeline, [pipeline_config]),
       # This needs to be called by the app hosting the application
       # worker(Otis.Startup, [Otis.State, Otis.Channels], restart: :transient)
     ]
-    supervise(children, strategy: :one_for_all)
+    supervise(children, strategy: :one_for_one)
   end
 
   def config(mod) do
